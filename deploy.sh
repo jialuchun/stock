004039aa375148fe70a2403313486b76cd4440e4
jialuchun <luchun.jia@jpinvestment.cn>
2020-04-01 01:21:11 +0800

    add file deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+###
+ # @Author: your name
+ # @Date: 2020-04-01 01:17:09
+ # @LastEditTime: 2020-04-01 01:17:10
+ # @LastEditors: Please set LastEditors
+ # @Description: In User Settings Edit
+ # @FilePath: /stock/deploy.sh
+ ###
